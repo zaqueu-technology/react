a97adb304c5cd7e17cfb88683f58dff4003bd773
Zaqueu <zaqueugithub@gmail.com>
2024-10-02 19:52:48 -0300

    addes modal and styled form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4} from 'uuid'
+
 
 function App() {
   const [role, setRole] = useState();
+  const [employees, setEmployees] = useState(
+    [
+      {
+        name: "Caleb",
+        role: "Dev",
+        img: "https://images.pexels.com/photos/220938/pexels-photo-220938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      },
+      {
+        name: "Toto",
+        role: "Designer",
+        img: "https://images.pexels.com/photos/1562983/pexels-photo-1562983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      },
+      {
+        name: "Ralph",
+        role: "CEO",
+        img: "https://images.pexels.com/photos/2253275/pexels-photo-2253275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      },
+      {
+        name: "Lince",
+        role: "Intern",
+        img: "https://images.pexels.com/photos/4681107/pexels-photo-4681107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      },
+      {
+        name: "Sake",
+        role: "Marketing",
+        img: "https://images.pexels.com/photos/4588047/pexels-photo-4588047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      },
+      {
+        name: "Foggy",
+        role: "Finances",
+        img: "https://images.pexels.com/photos/1684504/pexels-photo-1684504.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+      }
+    ]
+  )
   const showEmployee = true;
   return (
-    <div className="App">
+    <div className="App bg-red-300">
       {showEmployee ? 
-        <>
-        <input type='text' onChange={(e)=>{
-          setRole(e.target.value);
-        }}/>
-          <Employee name="Caleb" role="CEO"/>
-          <Employee name="John" role="Intern"/>
-          <Employee name="Carl" role={role}/>
-        </>
+        
+        <div className="flex flex-wrap justify-center">
+          {employees.map((employee) => {
+            console.log(uuidv4());
+            return (
+              <Employee 
+                key = {uuidv4()}
+                name = {employee.name}
+                role = {employee.role}
+                img = {employee.img} 
+              />
+            )
+          })}
+        </div>
+        
         :
         <p>You don't have permission!</p>
       }
@@ -24,3 +67,6 @@ function App() {
 }
 
 export default App;
+
+
+// O segundo return só é necessário se você estiver criando uma função dentro de outra função que também precise retornar JSX.
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,25 @@
+import EditEmployee from "./EditEmployee";
 
 export default function Employee(p){
   return (
-    <>
-      <h3>Employee {p.name ? p.name : "Anonymous"}</h3>
-      <p>Role: {p.role ? p.role : 'No role'}</p>
-    </>
+    <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <img 
+        class="object-cover h-[100px] w-[100px] block rounded-full sm:mx-0 sm:shrink-0" 
+        src={p.img} 
+        alt="Employee's Face" 
+      />
+      <div class="text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg text-black font-semibold">
+            {p.name}
+          </p>
+          <p class="text-slate-500 font-medium">
+            {p.role}
+          </p>
+        </div>
+        <EditEmployee/>
+        
+      </div>
+    </div>
   )
 }
